d88cecb6daee2f0c79ae4c0ce74a06e38a7d3e15
mangaz900 <shamoel1990@gmail.com>
2025-11-24 23:16:23 +0100

    Add fore-efter-2.jpg as fifth image in listicle

--- pages/listicles/HairGrowthRollOn.tsx
+++ pages/listicles/HairGrowthRollOn.tsx
@@ -295,7 +295,7 @@ const HairGrowthRollOn: React.FC = () => {
             <div className="w-full md:w-1/2 lg:w-2/5 flex-shrink-0">
               <div className="aspect-square bg-medical-50 rounded-xl overflow-hidden border border-medical-100">
                 <img 
-                  src="/listicle-point-5.jpg" 
+                  src="/fore-efter-2.jpg" 
                   alt="Förebyggande"
                   className="w-full h-full object-cover"
                   onError={(e) => {
